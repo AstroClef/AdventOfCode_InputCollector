55566583eb81deff50959c8d6accdf9dfccb6287
AstroClef <54979509+AstroClef@users.noreply.github.com>
2024-12-15 23:51:21 -0500

    Initialization of Project

---
+++ AOC_InputCollector/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AOC_InputCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
